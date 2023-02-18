281cd9cfe226eb8e6b4aab0e7e7457b301f26af2
Ruan Pedro Batista da Silva <121466178+RuanDevz@users.noreply.github.com>
2023-02-18 12:02:43 -0300

    botao clicar

---
+++ script.js
@@ -0,0 +1,13 @@
+const mode = document.getElementById('moon');
+
+mode.addEventListener('click', clicar);
+
+function clicar() {
+  if (mode.classList.contains('fa-moon')) {
+    mode.classList.remove('fa-moon');
+    mode.classList.add('fa-sun');
+  } else {
+    mode.classList.remove('fa-sun');
+    mode.classList.add('fa-moon');
+  }
+}
