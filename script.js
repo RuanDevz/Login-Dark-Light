4774b306b1bf8070d64419e9355cfdaa07911fa6
Ruan Pedro Batista da Silva <121466178+RuanDevz@users.noreply.github.com>
2023-02-19 10:29:52 -0300

    etntando

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const mode = document.getElementById('moon')
 mode.addEventListener('click',clicar)
 
 function clicar(){
+
+  const dark = document.getElementById('dark')
+
   if(mode.classList.contains('fa-moon')) {
     mode.classList.remove('fa-moon');
     mode.classList.add('fa-sun');
