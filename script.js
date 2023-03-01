b9a3aeaca0c91ebbca5d046530e0b41bcc5ec53f
Ruan Pedro Batista da Silva <121466178+RuanDevz@users.noreply.github.com>
2023-03-01 10:50:03 -0300

    pok

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ mode.addEventListener('click',clicar)
 function clicar(){
 
   const dark = document.getElementById('dark')
-
+  
   if(mode.classList.contains('fa-moon')) {
     mode.classList.remove('fa-moon');
     mode.classList.add('fa-sun');
