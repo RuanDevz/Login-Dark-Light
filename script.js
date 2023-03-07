7884dcab5317e4b439f77726af604e8a91ea6877
Ruan Pedro Batista da Silva <121466178+RuanDevz@users.noreply.github.com>
2023-03-06 22:42:04 -0300

    to muito felizzzzz

--- script.js
+++ script.js
@@ -4,13 +4,29 @@ mode.addEventListener('click',clicar)
 
 function clicar(){
 
-  const dark = document.getElementById('dark')
+  const container = document.getElementById('dark')
+  const login = document.getElementById('login')
+  const name =  document.getElementById('name','email','pass')
+  const forgot = document.getElementById('forgot')
+
+
   
   if(mode.classList.contains('fa-moon')) {
     mode.classList.remove('fa-moon');
     mode.classList.add('fa-sun');
+    container.style.background = '#312d37'
+    login.style.color = '#FFF'
+    name.style.color = '#FFF'
+    forgot.style.color = '#FFF'
+    mode.style.color = '#FFF'
   } else {
+    container.style.background = '#FFF'
+    login.style.color = '#000'
+    name.style.color = '#000'
+    forgot.style.color = '#000'
+    mode.style.color = '#000'
     mode.classList.remove('fa-sun');
     mode.classList.add('fa-moon');
   }
 }
+
